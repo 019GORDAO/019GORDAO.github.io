18a79317a3d8ad064ba2457f635c897175e4f168
019GORDAO <107517427+019GORDAO@users.noreply.github.com>
2022-06-16 22:31:44 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+
+$nick = addcslashes($_POST [ 'name' ])
+$message = addcslashes($_POST [ 'messa'])
+
+$to = "019ceifador@gmail.com";
+$subjet = "ideias pra vídeo";
+$body = "NICK: ".$nick."\n"
+"MESSAGE: ".$message;
+$heador = "019ceifador@gmail.com"."\r\n"."Reply-To:".$email."\r\n"."X=Mailer:PHP/".phpversion();
+
+if(mail($to,$subjet,$body,$heador)){
+  echo("sua ideia de vídeo ta na mão do 019 ?")
+} else {
+  echo("sua ideia de video deu problema tenta mais tarde");
+}
+?>
